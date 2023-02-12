4fe8eb42ea1170696bb468e13d127aaaa4a7b301
Nitish Kumar Tiwari <94640716+Nitishtiwari17@users.noreply.github.com>
2023-02-13 01:03:59 +0530

    Add files via upload

---
+++ Switchcase.php
@@ -0,0 +1,27 @@
+<?php
+//switch case
+$var1=20;
+$var2=10;
+$var3=28;
+$var4=12;
+switch($var1>$var2 && $var1>$var3 && $var1>$var4){
+    case 1:
+         echo "$var1 is greater";
+         break;
+    case 0: switch($var2>$var1 && $var2>$var3 && $var2>$var4)
+    {
+        case 1:
+          echo "$var2 is greater";
+          break;
+        case 0: switch($var3>$var1 && $var3>$var2 && $var3>$var4){
+          case 1:
+            echo "$var3 is greater";
+            break;
+          case 0:
+              echo "$var4 is greater";
+                
+          } 
+        } 
+        default : "all variable are equal";    
+    } 
+    ?> 
